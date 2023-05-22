fb9cd54ff5fb0a044037b5449b435ab5553b8653
christiansulaiman <csulaiman@ucsd.edu>
2023-05-21 19:18:16 -0700

    added readme + fixed sw.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 7 - Starter
+## Christian Sulaiman
+https://christiansulaiman.github.io/Lab7_Starter/
--- sw.js
+++ sw.js
@@ -41,12 +41,14 @@ self.addEventListener('fetch', function (event) {
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
   event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
-        return fetchedResponse;
+    if(!event.request.url.startsWith('chrome-extension')) {
+      return cache.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+          cache.put(event.request, fetchedResponse.clone());
+          return fetchedResponse;
+        })
       })
-    })
+    }
   }));
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
